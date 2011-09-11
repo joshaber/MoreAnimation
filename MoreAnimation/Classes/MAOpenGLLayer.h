22ded8c7305beb8935a9b6fc4b318a7e36a5e013
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-09-10 23:10:14 -0700

    Adding MAOpenGLLayer stub class

---
+++ MoreAnimation/Classes/MAOpenGLLayer.h
@@ -0,0 +1,13 @@
+//
+//  MAOpenGLLayer.h
+//  MoreAnimation
+//
+//  Created by Justin Spahr-Summers on 2011-09-10.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MALayer.h"
+
+@interface MAOpenGLLayer : MALayer
+
+@end
---
+++ MoreAnimation/Classes/MAOpenGLLayer.m
@@ -0,0 +1,13 @@
+//
+//  MAOpenGLLayer.m
+//  MoreAnimation
+//
+//  Created by Justin Spahr-Summers on 2011-09-10.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MAOpenGLLayer.h"
+
+@implementation MAOpenGLLayer
+
+@end
