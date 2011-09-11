6911e27044a82665056e4db75a0ac2d51aa3add4
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-09-11 15:46:42 -0700

    Moved MoreAnimation application code into a new OSXDemo project

---
+++ Demos/OSXDemo/OSXDemo/AppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.m
+//  OSXDemo
+//
+//  Created by Justin Spahr-Summers on 2011-09-11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+@synthesize openGLView;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
