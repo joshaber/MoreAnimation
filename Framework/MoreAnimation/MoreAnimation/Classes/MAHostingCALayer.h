67d94c4cbdabaa0e9a4c56db02bce0e950fe192b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2011-09-29 21:33:23 -0700

    Added MAHostingCALayer class for displaying an MALayer tree within a CALayer

---
+++ Framework/MoreAnimation/MoreAnimation/Classes/MAHostingCALayer.h
@@ -0,0 +1,25 @@
+//
+//  MAHostingCALayer.h
+//  MoreAnimation
+//
+//  Created by Justin Spahr-Summers on 2011-09-29.
+//  Copyright (c) 2011 Übermind, Inc. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@class MALayer;
+
+/**
+ * A Core Animation layer that hosts a More Animation layer tree.
+ */
+@interface MAHostingCALayer : CALayer
+/**
+ * The More Animation layer tree to display. The layer will be set to fill the
+ * entire bounds of the receiver.
+ * 
+ * Most non-geometrical Core Animation properties accessed on the receiver will
+ * simply call through to the properties on this layer.
+ */
+@property (strong) MALayer *MALayer;
+@end
---
+++ Framework/MoreAnimation/MoreAnimation/Classes/MAHostingCALayer.m
@@ -0,0 +1,85 @@
+//
+//  MAHostingCALayer.m
+//  MoreAnimation
+//
+//  Created by Justin Spahr-Summers on 2011-09-29.
+//  Copyright (c) 2011 Übermind, Inc. All rights reserved.
+//
+
+#import <MoreAnimation/MAHostingCALayer.h>
+#import <MoreAnimation/MALayer.h>
+#import <libkern/OSAtomic.h>
+#import "EXTScope.h"
+
+@interface MAHostingCALayer () {
+	MALayer *m_MALayer;
+	OSSpinLock m_MALayerSpinLock;
+}
+
+@end
+
+@implementation MAHostingCALayer
+
+#pragma mark Properties
+
+@synthesize MALayer = m_MALayer;
+
+- (id)contents {
+  	return self.MALayer.contents;
+}
+
+- (void)setContents:(id)value {
+  	self.MALayer.contents = value;
+}
+
+- (MALayer *)MALayer {
+ 	OSSpinLockLock(&m_MALayerSpinLock);
+	@onExit {
+		OSSpinLockUnlock(&m_MALayerSpinLock);
+	};
+
+  	return m_MALayer;
+}
+
+- (void)setMALayer:(MALayer *)layer {
+  	OSSpinLockLock(&m_MALayerSpinLock);
+	m_MALayer = layer;
+  	OSSpinLockUnlock(&m_MALayerSpinLock);
+}
+
+- (BOOL)needsDisplayOnBoundsChange {
+  	return self.MALayer.needsDisplayOnBoundsChange;
+}
+
+- (void)setNeedsDisplayOnBoundsChange:(BOOL)value {
+  	self.MALayer.needsDisplayOnBoundsChange = value;
+}
+
+- (BOOL)isOpaque {
+  	return self.MALayer.opaque;
+}
+
+- (void)setOpaque:(BOOL)value {
+  	self.MALayer.opaque = value;
+}
+
+#pragma mark Layout
+
+- (void)layoutSublayers {
+  	self.MALayer.frame = self.bounds;
+}
+
+#pragma mark Rendering and drawing
+
+- (void)display {
+  	// don't draw anything -- depend on MALayer caching
+}
+
+- (void)drawInContext:(CGContextRef)context {
+  	[self.MALayer drawInContext:context];
+}
+
+- (void)renderInContext:(CGContextRef)context {
+  	[self.MALayer renderInContext:context];
+}
+@end
